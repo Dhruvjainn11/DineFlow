7175fd7b229c8314e31265dbc297bd3e454c3f51
Dhruvjainn11 <dhruvjainn25@gmail.com>
2025-08-18 13:59:35 +0530

    random shit

--- AdminPanel/src/components/ProtectedRoute.jsx
+++ AdminPanel/src/components/ProtectedRoute.jsx
@@ -1,4 +1,5 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import React from "react";
 import {useAuth} from "../context/AuthContext";
 
 function ProtectedRoute({ allowedRole }) {
