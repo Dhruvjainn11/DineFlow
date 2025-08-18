edb3aafaed186b7cdacddc5d98bbeb147e94fd5a
Dhruvjainn11 <dhruvjainn25@gmail.com>
2025-08-18 10:10:21 +0530

    update gitignore

--- AdminPanel/src/utils/socket.js
+++ AdminPanel/src/utils/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:5000", {
+export const socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:5000","https://dineflow-adminpanel.vercel.app",
+      "https://dineflow-customer.vercel.app", {
   withCredentials: true,
   transports: ["websocket"], // optional but avoids fallback issues
 });
